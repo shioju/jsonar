96f0e85825ac443b77efbe48ac2e6f16b2834c9e
Sheng Hau <shioju12@gmail.com>
2017-10-16 13:36:44 +0800

    Implement #get_query
    - terminate CLI gracefully when Ctrl-c is pressed

--- lib/jsonar/cli.rb
+++ lib/jsonar/cli.rb
@@ -16,13 +16,21 @@ module Jsonar
 
     private_class_method
 
+    trap('INT') do
+      exit
+    end
+
     def self.parse_args(files)
       return files unless files.empty?
       puts 'Please specify a JSON file to search in'
       puts 'Usage: jsonar [FILE]...'
     end
 
-    def self.get_query; end
+    def self.get_query
+      puts 'What are you looking for? (press Ctrl-c to exit)'
+      query = STDIN.gets
+      query && query.chomp || query
+    end
 
     def self.search(_index, _query); end
   end
--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -2,7 +2,7 @@ require 'jsonar/cli'
 require 'jsonar/version'
 
 RSpec.describe Jsonar::CLI do
-  describe 'run' do
+  describe '#run' do
     it 'prompts for input file if not specified' do
       expect { Jsonar::CLI.run }.to output(/specify a JSON file/).to_stdout
     end
@@ -12,9 +12,23 @@ RSpec.describe Jsonar::CLI do
     end
   end
 
-  describe 'get_input' do
+  describe '#get_input' do
     it 'returns the input file path when provided' do
-      expect(Jsonar::CLI.parse_args(['foo', 'bar'])).to eq(['foo', 'bar'])
+      expect(Jsonar::CLI.parse_args(%w[foo bar])).to eq(%w[foo bar])
+    end
+  end
+
+  describe '#get_query' do
+    it 'prompts the user' do
+      allow(STDIN).to receive(:gets).and_return('')
+      expect { Jsonar::CLI.get_query }
+        .to output(/What are you looking for/).to_stdout
+    end
+
+    it 'strips trailing newline' do
+      allow(STDOUT).to receive(:puts)
+      allow(STDIN).to receive(:gets).and_return("foo\n")
+      expect(Jsonar::CLI.get_query).to eq('foo')
     end
   end
 end
